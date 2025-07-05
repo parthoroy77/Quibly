2088554905ebc4a039960fc37883ef56a811e78e
Partho Roy <parthoroy117@gmail.com>
2025-07-05 07:53:46 +0000

    fix: auth middleware errors

--- apps/api/src/app/middleware/auth.middleware.ts
+++ apps/api/src/app/middleware/auth.middleware.ts
@@ -1,5 +1,5 @@
-import prisma from "@quibly/prisma/client";
-import { TUserRoles } from "@quibly/utils/types";
+import db from "@quibly/database";
+import { UserRole } from "@quibly/database/client";
 import { NextFunction, Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import config from "../config";
@@ -8,10 +8,10 @@ import asyncHandler from "../handlers/asyncHandler";
 import { verifyToken } from "../modules/auth/auth.utils";
 
 // Helper function to verify session
-const verifySession = async (sessionToken: string, userId: number) => {
-  const session = await prisma.session.findFirst({
+const verifySession = async (token: string, userId: string) => {
+  const session = await db.session.findFirst({
     where: {
-      sessionToken,
+      token,
       userId,
       expiresAt: {
         gt: new Date(),
@@ -27,11 +27,11 @@ const verifySession = async (sessionToken: string, userId: number) => {
 };
 
 // Helper function to fetch the user
-const fetchUser = async (userId: number) => {
-  const user = await prisma.user.findUnique({
+const fetchUser = async (userId: string) => {
+  const user = await db.user.findUnique({
     where: {
       id: userId,
-      status: "ACTIVE",
+      status: "active",
       emailVerified: true,
     },
   });
@@ -44,7 +44,7 @@ const fetchUser = async (userId: number) => {
 };
 
 // Auth middleware
-const authMiddleware = (...requiredRoles: TUserRoles[]) => {
+const authMiddleware = (...requiredRoles: UserRole[]) => {
   return asyncHandler(async (req: Request, _res: Response, next: NextFunction) => {
     const session_token = req.cookies.session_token || req.headers.authorization?.split(" ")[1];
 
@@ -60,12 +60,12 @@ const authMiddleware = (...requiredRoles: TUserRoles[]) => {
     }
 
     // Verify session validity
-    const session = await verifySession(session_token!, Number(decode.userId));
+    const session = await verifySession(session_token!, decode.userId as string);
 
     // Fetch the user and check account status
     const user = await fetchUser(session.userId);
     // Check if the user has the required role(s)
-    if (requiredRoles.length > 0 && !requiredRoles.includes((decode.role && user.role) as TUserRoles)) {
+    if (requiredRoles.length > 0 && !requiredRoles.includes((decode.role && user.role) as UserRole)) {
       throw new ApiError(StatusCodes.FORBIDDEN, "You do not have the required permissions to access this resource.");
     }
 
